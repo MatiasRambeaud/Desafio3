e8d74a824587fce3207f34b57a968a6cad068f58
MatiasRambeaud <92528330+MatiasRambeaud@users.noreply.github.com>
2024-07-07 17:11:17 -0300

    Add files via upload

--- src/Routes/carts.router.js
+++ src/Routes/carts.router.js
@@ -3,139 +3,105 @@ import cartsManager from "../Managers/mongo/CartsManager.js";
 import productsManager from "../Managers/mongo/ProductsManager.js";
 
 const router = Router();
+const CartsManager = new cartsManager();
 
+//Get cart data
 router.get("/:cid", async(req, res) => {
     const cid = req.params.cid;
-    const cart = await cartsManager.findOne({ _id: cid }).populate("products.product");
+
+    const cart = await CartsManager.getCart(cid);
     if (!cart) {
         res.send({ status: "error", error: "Cart not found" });
     }
     res.send({ status: "success", payload: cart });
 })
 
+//Create a new cart
 router.post("/", async(req, res) => {
-    const cart = {
-        products: []
-    };
-    const addCart = await cartsManager.create(cart);
-    if (!addCart) {
+    const result = await CartsManager.addCart();
+    if(!result){
         res.send({ status: "error", error: "Could not create cart" });
     }
-    res.send({ status: "success", payload: addCart });
+    res.send({ status: "success", payload: result });
 })
 
+//Add a new product to the cart
 router.post("/:cid/products/:pid", async(req, res) => {
     const cid = req.params.cid;
     const pid = req.params.pid;
-    const quantity = req.body.quantity || 1;
-
-    const cart = await cartsManager.findOne({ _id: cid });
-    if (!cart) {
+    const qty = req.body.quantity || 1;
+    const result = await CartsManager.addProduct(cid,pid,qty);
+    if(result=="no cart"){
         res.send({ status: "error", error: "Cart not found" });
     }
-    const product = await productsManager.findOne({ _id: pid });
-    if (!product) {
+    if (result=="no product") {
         res.send({ status: "error", error: "Product not found" });
     }
-
-    let exists = false;
-    cart.products.forEach(eachproduct => {
-        if (eachproduct.product._id == pid) {
-            eachproduct.quantity += quantity;
-            exists = true;
-        }
-    });
-    if (!exists) {
-        cart.products.push({ product: product._id, quantity: quantity });
-        const result = await cartsManager.updateOne({ _id: cid }, { products: cart.products });
-        if (!result) {
-            res.send({ status: "error", error: "Couldnt add product" });
-        }
-        res.send({ status: "success", payload: result });
-    } else {
-        const result = await cartsManager.updateOne({ _id: cid }, { $set: { products: cart.products } });
-        if (!result) {
-            res.send({ status: "error", error: "Couldnt add product" });
-        } else {
-            res.send({ status: "success", payload: result });
-        }
+    if(result=="server error"){
+        res.send({ status: "error", error: "Couldnt add product" });
     }
+    res.send({ status: "success", payload: result });
 
 })
 
+//Update entire cart
 router.put("/:cid", async(req, res) => {
     const cid = req.params.cid;
     const data = req.body;
 
-    const result = await cartsManager.updateOne({ _id: cid }, { products: data });
+    const result = await CartsManager.updateCart(cid,data);
     if (!result) {
         res.send({ status: "error", error: "Could not edit products" });
     }
     res.send({ status: "success", payload: result });
 })
+
+//Update product quantity
 router.put("/:cid/products/:pid", async(req, res) => {
     const cid = req.params.cid;
     const pid = req.params.pid;
-    const quantity = req.body.quantity;
+    const qty = req.body.quantity;
 
-    const cart = await cartsManager.findOne({ _id: cid });
-    if (!cart) {
+    const result = await CartsManager.updateProductQuantity(cid,pid,qty);
+    if(result=="no cart"){
         res.send({ status: "error", error: "Cart not found" });
     }
-    const product = await productsManager.findOne({ _id: pid });
-    if (!product) {
+    if(result=="no product"){
         res.send({ status: "error", error: "Product not found" });
     }
-
-    cart.products.forEach(eachproduct => {
-        if (eachproduct.product._id == pid) {
-            eachproduct.quantity = quantity;
-        }
-    });
-
-    const result = await cartsManager.updateOne({ _id: cid }, cart);
-    if (!result) {
+    if (result=="server error") {
         res.send({ status: "error", error: "Could not edit products" });
     }
     res.send({ status: "success", payload: result });
 })
 
+//Delete specific product
 router.delete("/:cid/products/:pid", async(req, res) => {
     const cid = req.params.cid;
     const pid = req.params.pid;
 
-    const cart = await cartsManager.findOne({ _id: cid });
-    if (!cart) {
+    const result = await CartsManager.deleteProduct(cid,pid);
+    if (result=="no cart") {
         res.send({ status: "error", error: "Cart not found" });
     }
-    const product = await productsManager.findOne({ _id: pid });
-    if (!product) {
+    if (result=="no product") {
         res.send({ status: "error", error: "Product not found" });
     }
-
-    cart.products.forEach(eachproduct => {
-        if (eachproduct.product._id == pid) {
-            cart.products.splice(eachproduct);
-        }
-    });
-
-    const result = await cartsManager.updateOne({ _id: cid }, cart);
-    if (!result) {
+    if (result=="server error") {
         res.send({ status: "error", error: "Could not delete product" });
     }
     res.send({ status: "success", payload: result });
 })
 
+//Empty cart
 router.delete("/:cid", async(req, res) => {
     const cid = req.params.cid;
 
-    const cart = await cartsManager.findOne({ _id: cid });
-    if (!cart) {
+    const result = await CartsManager.DeleteAllProducts(cid);
+    if (result=="no cart") {
         res.send({ status: "error", error: "Cart not found" });
     }
-
-    const result = await cartsManager.updateOne({ _id: cid }, { $set: { products: [] } });
-    if (!result) {
+    if (result=="server error") {
         res.send({ status: "error", error: "Could not delete product" });
     }
     res.send({ status: "success", payload: result });
--- src/Routes/products.router.js
+++ src/Routes/products.router.js
@@ -1,103 +1,65 @@
 import { Router } from "express";
 import productsManager from "../Managers/mongo/ProductsManager.js";
-const router = Router();
 
+const router = Router();
+const ProductsManager = new productsManager();
+
+//Get product data
 router.get("/:page", async(req, res) => {
-    const page = req.params.page.toString() || 1;
-    const products = await productsManager.paginate({}, { page: page, limit: 10 })
-    if (!products) {
-        res.send({ status: error, send: (500), error: error });
+    const p = req.params.page.toString() || 1;
+    const result = await ProductsManager.getProducts(p);
+    if (result=="server error") {
+        res.send({ status: "error", error: "Server error" });
     }
 
-    const prevLink = () => {
-        if (products.hasPrevPage) {
-            return `http://localhost:8080/api/products/${products.page-1}`;
-        } else {
-            return null;
-        }
-    };
-    const nextLink = () => {
-        if (products.hasNextPage) {
-            return `http://localhost:8080/api/products/${products.page+1}`;
-        } else {
-            return null;
-        }
-    };
     res.send({
         status: "success",
-        payload: products.docs,
-        totalPages: products.totalPages,
-        prevPage: products.prevPage,
-        nextPage: products.nextPage,
-        page: products.page,
-        hasPrevPage: products.hasPrevPage,
-        hasNextPage: products.hasNextPage,
-        prevLink: prevLink(),
-        nextLink: nextLink()
+        payload: result.result,
+        totalPages: result.totalPages,
+        prevPage: result.prevPage,
+        nextPage: result.nextPage,
+        page: result.page,
+        hasPrevPage: result.hasPrevPage,
+        hasNextPage: result.hasNextPage,
+        prevLink: result.prevLink,
+        nextLink: result.nextLink
     });
 })
 
+//Create product
 router.post("/", async(req, res) => {
     const data = req.body;
     if (!data.title || !data.description || !data.code || !data.price || !data.category) {
         res.send({ status: "error", error: "Missing product values" })
-    }
-    const newProduct = {
-        title: data.title,
-        description: data.description,
-        code: data.code,
-        price: data.price,
-        stock: data.stock || 1,
-        category: data.category,
-        status: data.status || true
-    }
-
-    const exists = await productsManager.findOne({ code: data.code });
-
-    if (exists) {
-        const updatedProduct = await productsManager.updateOne({ code: data.code }, { stock: exists.stock + newProduct.stock });
-        console.log("Product stock incremented successfully");
-        res.send({ status: "success", payload: updatedProduct });
-    } else {
-        const product = await productsManager.create(newProduct);
-        if (!product) {
+    }else{
+        const result = ProductsManager.addProduct(data);
+        if(!result){
             res.send({ status: "error", error: "Could not create product" });
-        } else {
-            console.log("Product created successfully");
-            res.send({ status: "success", payload: product });
         }
+        res.send({ status: "success", payload: result });
     }
 })
 
+//Update product
 router.put("/:pid", async(req, res) => {
     const pid = req.params.pid;
     const data = req.body;
 
-    const update = {
-        title: data.title,
-        description: data.description,
-        code: data.code,
-        price: data.price,
-        stock: data.stock,
-        category: data.category,
-        status: data.status
-    }
-    const updatedProduct = await productsManager.updateOne({ _id: pid }, update);
-    if (!updatedProduct) {
+    const result = await ProductsManager.updateProduct(pid,data);
+    if (result=="server error") {
         res.send({ status: "error", error: "Could not update product" })
     }
-    console.log("Product updated successfully");
-    res.send({ status: "success", payload: updatedProduct });
+    res.send({ status: "success", payload: result });
 })
 
+//Delete product
 router.delete("/:pid", async(req, res) => {
     const pid = req.params.pid;
-    const deletedProduct = await productsManager.deleteOne({ _id: pid });
-    if (!deletedProduct) {
+    const result = await ProductsManager.deleteProduct(pid);
+    if (result=="server error") {
         res.send({ status: "error", error: "Could not delete product" })
     }
-    console.log("Product deleted successfully");
-    res.send({ status: "success", payload: deletedProduct });
+    res.send({ status: "success", payload: result });
 })
 
 export default router;
